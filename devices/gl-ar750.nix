67c21e51d7113483173bdeb4143441ffd6f853ac
Daniel Barlow <dan@telent.net>
2022-10-05 21:58:39 +0100

    gl-ar750: boots

--- devices/gl-ar750.nix
+++ devices/gl-ar750.nix
@@ -31,46 +31,56 @@
       "MIPS_ELF_APPENDED_DTB" = "y";
     };
     config = {
+      CPU_LITTLE_ENDIAN= "n";
+      CPU_BIG_ENDIAN= "y";
+      ATH79 = "y";
       MIPS_ELF_APPENDED_DTB = "y";
+
+      # "empty" initramfs source should create an initial
+      # filesystem that has a /dev/console node and not much
+      # else.  Note that pid 1 is started *before* the root
+      # filesystem is mounted and it expects /dev/console to
+      # be present already
+      BLK_DEV_INITRD = "y";
+      INITRAMFS_SOURCE = "\"\"";
 
       # this is all copied from nixwrt ath79 config. Clearly not all
       # of it is device config, some of it is wifi config or
       # installation method config or ...
-      "BLK_DEV_INITRD" = "n";
+
       "CMDLINE_PARTITION" = "y";
       "DEBUG_INFO" = "y";
-      "DEVTMPFS" = "y";
       "EARLY_PRINTK" = "y";
       "FW_LOADER" = "y";
       # we don't have a user helper, so we get multiple 60s pauses
       # at boot time unless we disable trying to call it
       "FW_LOADER_USER_HELPER" = "n";
-      "IMAGE_CMDLINE_HACK" = "n";
-      "IP_PNP" = "y";
-      "JFFS2_FS" = "n";
+
+      # "IMAGE_CMDLINE_HACK" = "n";
+
       "MODULE_SIG" = "y";
       "MTD_CMDLINE_PARTS" = "y";
-      "MTD_SPLIT_FIRMWARE" = "y";
+#      "MTD_SPLIT_FIRMWARE" = "y";
       "PARTITION_ADVANCED" = "y";
       "PRINTK_TIME" = "y";
       "SQUASHFS" = "y";
       "SQUASHFS_XZ" = "y";
-      "ASN1" = "y";
-      "ASYMMETRIC_KEY_TYPE" = "y";
-      "ASYMMETRIC_PUBLIC_KEY_SUBTYPE" = "y";
-      "CRC_CCITT" = "y";
-      "CRYPTO" = "y";
-      "CRYPTO_ARC4" = "y";
-      "CRYPTO_CBC" = "y";
-      "CRYPTO_CCM" = "y";
-      "CRYPTO_CMAC" = "y";
-      "CRYPTO_GCM" = "y";
-      "CRYPTO_HASH_INFO" = "y";
-      "CRYPTO_LIB_ARC4" = "y";
-      "CRYPTO_RSA" = "y";
-      "CRYPTO_SHA1" = "y";
-      "ENCRYPTED_KEYS" = "y";
-      "KEYS" = "y";
+      # "ASN1" = "y";
+      # "ASYMMETRIC_KEY_TYPE" = "y";
+      # "ASYMMETRIC_PUBLIC_KEY_SUBTYPE" = "y";
+      # "CRC_CCITT" = "y";
+      # "CRYPTO" = "y";
+      # "CRYPTO_ARC4" = "y";
+      # "CRYPTO_CBC" = "y";
+      # "CRYPTO_CCM" = "y";
+      # "CRYPTO_CMAC" = "y";
+      # "CRYPTO_GCM" = "y";
+      # "CRYPTO_HASH_INFO" = "y";
+      # "CRYPTO_LIB_ARC4" = "y";
+      # "CRYPTO_RSA" = "y";
+      # "CRYPTO_SHA1" = "y";
+      # "ENCRYPTED_KEYS" = "y";
+      # "KEYS" = "y";
     };
   };
   outputs.default = "combined-image";
