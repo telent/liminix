0a46ba7fc3dd1a727562da0c2884cfad7d746429
Daniel Barlow <dan@telent.net>
2022-10-19 17:36:31 +0100

    move some wifi-related comments into wlan module

--- modules/wlan.nix
+++ modules/wlan.nix
@@ -7,14 +7,32 @@ let
 in {
   config = {
     kernel = rec {
-      config = {
+      checkedConfig = {
         CFG80211= "y";
         MAC80211= "y";
         MAC80211_MESH= "y";
         RFKILL= "y";
         WLAN = "y";
+        # if/when we switch to using backported mac80211 drivers built
+        # as modules, based on nixwrt code we expect we will need this config
+        # to enable them
+        # "ASN1" = "y";
+        # "ASYMMETRIC_KEY_TYPE" = "y";
+        # "ASYMMETRIC_PUBLIC_KEY_SUBTYPE" = "y";
+        # "CRC_CCITT" = "y";
+        # "CRYPTO" = "y";
+        # "CRYPTO_ARC4" = "y";
+        # "CRYPTO_CBC" = "y";
+        # "CRYPTO_CCM" = "y";
+        # "CRYPTO_CMAC" = "y";
+        # "CRYPTO_GCM" = "y";
+        # "CRYPTO_HASH_INFO" = "y";
+        # "CRYPTO_LIB_ARC4" = "y";
+        # "CRYPTO_RSA" = "y";
+        # "CRYPTO_SHA1" = "y";
+        # "ENCRYPTED_KEYS" = "y";
+        # "KEYS" = "y";
       };
-      checkedConfig = config;
     };
   };
 }
