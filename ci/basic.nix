eb4efab6a215bf03cf5aab10d4ac909e83e9c148
Daniel Barlow <dan@telent.net>
2023-01-28 23:18:28 +0000

    might be a hydra config

---
+++ ci/basic.nix
@@ -0,0 +1,11 @@
+{ ... }:
+let
+  # <nixpkgs> is set to the value designated by the nixpkgs input of the
+  # jobset configuration.
+  pkgs = (import <nixpkgs> {});
+  device =  import <liminix/devices/qemu>;
+  liminix-config = <liminix/tests/smoke/configuration.nix>;
+  liminix = import <liminix> { inherit device liminix-config; };
+in {
+  inherit (liminix.outputs) squashfs;
+}
