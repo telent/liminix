cb6cc4b858c7d44684716a2a66a554ea900bad55
Daniel Barlow <dan@telent.net>
2023-01-29 12:42:15 +0000

    add jobs to jobset so we can see more easily what fails

--- ci/basic.nix
+++ ci/basic.nix
@@ -7,5 +7,5 @@ let
   liminix-config = import <liminix/tests/smoke/configuration.nix>;
   liminix = import <liminix> { inherit device liminix-config; };
 in {
-  inherit (liminix.outputs) squashfs;
+  inherit (liminix.outputs) squashfs kernel default manifest;
 }
