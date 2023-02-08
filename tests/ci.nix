320d436c65e3c0884167cfa1d82749864b982b35
Daniel Barlow <dan@telent.net>
2023-02-08 23:19:18 +0000

    add wlan test to ci

--- tests/ci.nix
+++ tests/ci.nix
@@ -1,5 +1,6 @@
 {
   smoke = import ./smoke/test.nix;
   pseudofiles = import ./pseudofiles/test.nix;
+  wlan = import ./wlan/test.nix;
   pppoe = import ./pppoe/test.nix;
 }
---
+++ tests/wlan/test.nix
@@ -0,0 +1,39 @@
+{
+  liminix
+, nixpkgs
+}:
+let img = (import liminix {
+      device = import "${liminix}/devices/qemu/";
+      liminix-config = ./configuration.nix;
+    }).outputs.default;
+    pkgs = import <nixpkgs> { overlays = [(import ../../overlay.nix)]; };
+    inherit (pkgs.pkgsBuildBuild)  mips-vm;
+in pkgs.runCommand "check" {
+  nativeBuildInputs = with pkgs; [
+    expect
+    mips-vm
+    socat
+  ] ;
+} ''
+killpid(){
+  if test -e $1 && test -d /proc/`cat $1` ; then
+    pid=$(cat $1)
+    kill $pid
+  fi
+}
+
+cleanup(){
+  killpid ./vm/pid
+}
+trap cleanup EXIT
+fatal(){
+    err=$?
+    echo "FAIL: command $(eval echo $BASH_COMMAND) exited with code $err"
+    exit $err
+}
+trap fatal ERR
+
+mkdir vm
+mips-vm --background ./vm ${img}/vmlinux ${img}/squashfs
+expect ${./wait-for-wlan.expect} > output && mv output $out
+''
