1a29e2ba0f85620932362fa039af02e7e978e5d0
Daniel Barlow <dan@telent.net>
2023-02-15 20:07:24 +0000

    use locally modified tftp.lua instead of upstream tufty

--- pkgs/tufted/default.nix
+++ pkgs/tufted/default.nix
@@ -5,23 +5,8 @@
 , makeWrapper
 } :
 let
-  tufty-lua = lua5_3.pkgs.buildLuaPackage {
-    pname = "tufty";
-    version = "1";
-    src = fetchFromGitHub {
-      owner = "telent";
-      repo = "tufty";
-      sha256 = "sha256-m5UEfcCNdG0Ku380cPhu1inNQmSfQJ5NcRIxLohUOh8=";
-      rev = "75c6d38713a82f4197f91dcb182a2e34f255bf7c";
-    };
-    buildPhase = ":";
-    installPhase = ''
-        mkdir -p "$out/share/lua/${lua5_3.luaversion}"
-        cp src/*.lua "$out/share/lua/${lua5_3.luaversion}/"
-      '';
-  };
   lua = lua5_3.withPackages (ps: with ps; [
-    tufty-lua luasocket luaposix fennel
+    luasocket luaposix fennel
   ]);
 in stdenv.mkDerivation {
   pname = "tufted";
@@ -34,7 +19,7 @@ in stdenv.mkDerivation {
   src = ./.;
   installPhase = ''
     mkdir -p $out/lib
-    cp tufted.fnl $out/lib
+    cp tftp.lua tufted.fnl $out/lib
     makeWrapper ${lua.pkgs.fennel}/bin/fennel \
       $out/bin/tufted \
       --prefix LUA_CPATH \; "${lua}/lib/lua/5.3/?.so" \
