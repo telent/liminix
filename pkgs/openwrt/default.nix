429ffa8e7dcd93aef12148d765adbf612ee2e85b
Daniel Barlow <dan@telent.net>
2023-03-19 09:55:39 +0000

    make applyPatches work for ramips

--- pkgs/openwrt/default.nix
+++ pkgs/openwrt/default.nix
@@ -10,32 +10,21 @@ let
     rev = "a5265497a4f6da158e95d6a450cb2cb6dc085cab";
     hash = "sha256-YYi4gkpLjbOK7bM2MGQjAyEBuXJ9JNXoz/JEmYf8xE8=";
   };
+  doPatch = family : ''
+    cp -av ${src}/target/linux/generic/files/* .
+    chmod -R u+w .
+    cp -av ${src}/target/linux/${family}/files/* .
+    chmod -R u+w .
+    patches() {
+      for i in $* ; do patch --batch --forward -p1 < $i ;done
+    }
+    patches ${src}/target/linux/generic/backport-5.15/*.patch
+    patches ${src}/target/linux/generic/pending-5.15/*.patch
+    patches ${src}/target/linux/generic/hack-5.15/*.patch
+    patches ${src}/target/linux/${family}/patches-5.15/*.patch
+  '';
 in {
   inherit src;
-  applyPatches.ath79 = writeShellScript "apply-patches-ath79" ''
-    cp -av ${src}/target/linux/generic/files/* .
-    chmod -R u+w .
-    cp -av ${src}/target/linux/ath79/files/* .
-    chmod -R u+w .
-    patches() {
-      for i in $* ; do patch --batch --forward -p1 < $i ;done
-    }
-    patches ${src}/target/linux/generic/backport-5.15/*.patch
-    patches ${src}/target/linux/generic/pending-5.15/*.patch
-    patches ${src}/target/linux/generic/hack-5.15/*.patch
-    patches ${src}/target/linux/ath79/patches-5.15/*.patch
-  '';
-  applyPatches.ramips = writeShellScript "apply-patches-ramips" ''
-    cp -av ${src}/target/linux/generic/files/* .
-    chmod -R u+w .
-    cp -av ${src}/target/linux/ramips/files/* .
-    chmod -R u+w .
-    patches() {
-      for i in $* ; do patch --batch --forward -p1 < $i ;done
-    }
-    patches ${src}/target/linux/generic/backport-5.15/*.patch
-    patches ${src}/target/linux/generic/pending-5.15/*.patch
-    patches ${src}/target/linux/generic/hack-5.15/*.patch
-    patches ${src}/target/linux/ramips/patches-5.15/*.patch
-  '';
+  applyPatches.ath79 = doPatch "ath79";
+  applyPatches.ramips = doPatch "ramips";
 }
