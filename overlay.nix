435a36f267204d5a87de6b848f20866b3fb59621
Daniel Barlow <dan@telent.net>
2023-06-20 20:06:36 +0100

    delete unused derivations in overlay

--- overlay.nix
+++ overlay.nix
@@ -47,8 +47,6 @@ extraPkgs // {
     ];
   });
 
-  # openssl is reqired by ntp
-
   rsyncSmall = prev.rsync.overrideAttrs(o: {
     configureFlags = o.configureFlags ++ [
       "--disable-openssl"
@@ -71,27 +69,6 @@ extraPkgs // {
       libseccomp = null;
     };
 
-  ntp =
-    let
-      openssl = prev.openssl.overrideAttrs(o: {
-        preInstall = ''
-          find . -name libcrypto.so.3 -ls
-          $STRIP lib*.so.*
-        '';
-      });
-      ntp_ = prev.ntp.overrideAttrs(o: {
-        outputs = [
-          "out"
-          "man"
-          "perllib"
-          "doc"
-        ];
-        postInstall = ''
-          mkdir -p $perllib
-          moveToOutput "share/ntp" $perllib
-        '';
-      });
-    in ntp_.override { inherit openssl; };
 
   strace = prev.strace.override { libunwind = null; };
 
@@ -184,41 +161,4 @@ extraPkgs // {
   });
 
   pppBuild = prev.ppp;
-  pppOld =
-    (prev.ppp.override {
-      libpcap = null;
-    }).overrideAttrs (o : {
-      stripAllList = [ "bin" ];
-      buildInputs = [];
-
-      # patches =
-      #   o.patches ++
-      #   [(final.fetchpatch {
-      #     name = "ipv6-script-options.patch";
-      #     url = "https://github.com/ppp-project/ppp/commit/874c2a4a9684bf6938643c7fa5ff1dd1cf80aea4.patch";
-      #     sha256 = "sha256-K46CKpDpm1ouj6jFtDs9IUMHzlRMRP+rMPbMovLy3o4=";
-      #   })];
-
-      postPatch = ''
-        sed -i -e  's@_PATH_VARRUN@"/run/"@'  pppd/main.c
-        sed -i -e  's@^FILTER=y@# FILTER unset@'  pppd/Makefile.linux
-        sed -i -e  's/-DIPX_CHANGE/-UIPX_CHANGE/g'  pppd/Makefile.linux
-      '';
-      buildPhase = ''
-        runHook preBuild
-        make -C pppd CC=$CC USE_TDB= HAVE_MULTILINK= USE_EAPTLS= USE_CRYPT=y
-        make -C pppd/plugins/pppoe CC=$CC
-        make -C pppd/plugins/pppol2tp CC=$CC
-        runHook postBuild;
-      '';
-      installPhase = ''
-        runHook preInstall
-        mkdir -p $out/bin $out/lib/pppd/2.4.9
-        cp pppd/pppd pppd/plugins/pppoe/pppoe-discovery $out/bin
-        cp pppd/plugins/pppoe/pppoe.so $out/lib/pppd/2.4.9
-        cp pppd/plugins/pppol2tp/{open,pppo}l2tp.so $out/lib/pppd/2.4.9
-        runHook postInstall
-      '';
-      postFixup = "";
-    });
 }
