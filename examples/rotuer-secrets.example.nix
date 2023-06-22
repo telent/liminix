5e45817f983e76e1514e36bcaee9f1ba805c19ec
Daniel Barlow <dan@telent.net>
2023-06-22 16:54:24 +0100

    example rotuer-secrets

---
+++ examples/rotuer-secrets.example.nix
@@ -0,0 +1,14 @@
+rec {
+  wpa_passphrase = "you bring light in";
+  l2tp = {
+    name = "abcde@a.1";
+    password = "NotMyIspPassword";
+  };
+  root = {
+    #  mkpasswd -m sha512crypt
+    passwd = "$6$6pt0mpbgcB7kC2RJ$kSBoCYGyi1.qxt7dqmexLj1l8E6oTZJZmfGyJSsMYMW.jlsETxdgQSdv6ptOYDM7DHAwf6vLG0pz3UD31XBfC1";
+    openssh.authorizedKeys.keys = [
+    ];
+  };
+  root_password = root.passwd;
+}
