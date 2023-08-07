6d619ee8b5a5c2b6cf26362daf8cbaf9d2bb82a8
Daniel Barlow <dan@telent.net>
2023-08-07 21:43:12 +0100

    add some missing descriptions

--- modules/hardware.nix
+++ modules/hardware.nix
@@ -7,13 +7,18 @@ in {
     };
     hardware = {
       dts = {
-        src = mkOption { type = types.path; };
+        src = mkOption {
+          type = types.path;
+          description = "Path to the device tree source (usually from OpenWrt)";
+        };
         includes = mkOption {
           default = [];
+          description = "List of directories to search for DTS includes (.dtsi files)";
           type = types.listOf types.path;
         };
       };
       defaultOutput = mkOption {
+        description = "\"Default\" output: what gets built for this device when outputs.default is requested. Typically this is \"flashimage\" or \"vmroot\"";
         type = types.nonEmptyStr;
       };
       flash = {
@@ -22,13 +27,31 @@ in {
         # kernel uimage and root fs. Not the entire flash, as
         # that often also contains the bootloader, data for
         # for wireless devices, etc
-        address = mkOption { type = types.str; };
-        size = mkOption { type = types.str; };
-        eraseBlockSize = mkOption { type = types.str; };
+        address = mkOption {
+          description = ''
+            Start address of whichever partition (often
+            called "firmware") we're going to overwrite with our
+            kernel uimage and root fs. Usually not the entire flash, as
+            we don't want to clobber the bootloader, calibration data etc
+          '';
+          type = types.str;
+        };
+        size = mkOption {
+          type = types.str;
+          description = "Size in bytes of the firmware partition";
+        };
+        eraseBlockSize = mkOption {
+          description = "Flash erase block size in bytes";
+          type = types.str;
+        };
       };
       loadAddress = mkOption { default = null; };
       entryPoint = mkOption { };
       radios = mkOption {
+        description = ''
+          Kernel modules (from mac80211 package) required for the
+          wireless devices on this board
+        '';
         type = types.listOf types.str;
         default = [];
         example = ["ath9k" "ath10k"];
