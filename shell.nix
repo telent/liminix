7fc5d2934dad50eaf0cd464d867684c94ed6132e
Daniel Barlow <dan@telent.net>
2023-09-08 21:01:39 +0100

    set FENNEL_PATH using absolute paths

--- shell.nix
+++ shell.nix
@@ -5,10 +5,11 @@ let
     liminix-config = ./vanilla-configuration.nix;
     inherit nixpkgs;
   });
+  here = builtins.toString ./.;
 in liminix.buildEnv.overrideAttrs (o: {
   nativeBuildInputs = o.nativeBuildInputs ++ [ (import nixpkgs {}).sphinx ] ;
   shellHook = ''
     publish(){  make -C doc html && rsync -azv doc/_build/html/ myhtic.telent.net:/var/www/blogs/www.liminix.org/_site/doc; }
   '';
-  FENNEL_PATH = "pkgs/?/init.fnl;pkgs/?.fnl";
+  FENNEL_PATH = "${here}/pkgs/?/init.fnl;${here}/pkgs/?.fnl";
 })
