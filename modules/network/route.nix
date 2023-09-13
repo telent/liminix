4fd1b5f08b4fa8e880fb432acab8caeb9ceaa105
Daniel Barlow <dan@telent.net>
2023-09-13 18:01:50 +0100

    er, "input" != output"

--- modules/network/route.nix
+++ modules/network/route.nix
@@ -7,7 +7,7 @@
 { target, via, interface ? null, metric }:
 let
   inherit (liminix.services) oneshot;
-  with_dev = if interface != null then "dev $(input ${interface} ifname)" else "";
+  with_dev = if interface != null then "dev $(output ${interface} ifname)" else "";
 in oneshot {
   name = "route-${target}-${builtins.substring 0 12 (builtins.hashString "sha256" "${via}-${if interface!=null then interface.name else ""}")}";
   up = ''
