f1c04c797978f9e404cd7a6c3d454f8028f1ba7f
Daniel Barlow <dan@telent.net>
2023-09-20 17:50:21 +0100

    extract mips kernel options to module

--- modules/arch/mips.nix
+++ modules/arch/mips.nix
@@ -3,6 +3,9 @@
   config = {
     kernel.config = {
       MIPS_ELF_APPENDED_DTB = "y";
+      MIPS_BOOTLOADER_CMDLINE_REQUIRE_COOKIE = "y";
+      MIPS_BOOTLOADER_CMDLINE_COOKIE = "\"liminix\"";
+      MIPS_CMDLINE_DTB_EXTEND = "y";
     };
     boot.commandLine = [
       "console=ttyS0,115200" # true of all mips we've yet encountered
