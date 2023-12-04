f08c10c8ba1a4de32d72777e6e4f05d7e828802c
Daniel Barlow <dan@telent.net>
2023-12-04 23:39:27 +0000

    patch u-boot to add ubifs support
    not that we're using it yet

--- overlay.nix
+++ overlay.nix
@@ -199,4 +199,17 @@ extraPkgs // {
   };
 
   strace = prev.strace.override { libunwind = null; };
+
+  ubootQemuArm = final.buildUBoot {
+    defconfig = "qemu_arm_defconfig";
+    extraMeta.platforms = ["armv7l-linux"];
+    filesToInstall = ["u-boot.bin"];
+    # enable looking for boot files on ubifs. this is unused at
+    # present, but added in the expectation of a future test
+    extraPatches = [ ./pkgs/u-boot/0001-add-ubifs-to-boot-targets.patch ];
+    extraConfig = ''
+      CONFIG_CMD_UBI=y
+      CONFIG_CMD_UBIFS=y
+    '';
+  };
 }
