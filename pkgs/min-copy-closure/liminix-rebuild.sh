76f11bcc93a9a865fd9fec393f0949cc013d77da
Daniel Barlow <dan@telent.net>
2024-03-28 21:36:48 +0000

    liminix-rebuild: remove -f flag from reboot call
    now we have timeouts in service definitions, shouldn't need this
    any more

--- pkgs/min-copy-closure/liminix-rebuild.sh
+++ pkgs/min-copy-closure/liminix-rebuild.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
 ssh_command=${SSH_COMMAND-ssh}
+
 if [ "$1" = "--no-reboot" ] ; then
     reboot="true"
     shift
@@ -20,7 +21,7 @@ if toplevel=$(nix-build "$@" -A outputs.systemConfiguration --no-out-link); then
     echo systemConfiguration $toplevel
     min-copy-closure $target_host $toplevel
     $ssh_command $target_host $toplevel/bin/install
-    $ssh_command  $target_host "sync; source /etc/profile; reboot -f"
+    $ssh_command  $target_host "sync; source /etc/profile; reboot"
 else
     echo Rebuild failed
 fi
