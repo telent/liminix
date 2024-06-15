9540fc264191afdf7719a56f054d127c02674b20
Daniel Barlow <dan@telent.net>
2024-06-15 15:04:56 +0100

    add writeAshScriptBin (forgot to add file)

---
+++ pkgs/write-ash-script/bin.nix
@@ -0,0 +1,13 @@
+{
+  writeScriptBin
+, lib
+}
+: name : { runtimeInputs ? [] } : text : writeScriptBin name ''
+#!/bin/sh
+set -o errexit
+set -o nounset
+set -o pipefail
+
+export PATH="${lib.makeBinPath runtimeInputs}:$PATH"
+${text}
+''
