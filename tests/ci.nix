e6b7d86381ba458c8ce83620faa90163608b42d3
Daniel Barlow <dan@telent.net>
2024-10-06 17:53:34 +0100

    sort lines

--- tests/ci.nix
+++ tests/ci.nix
@@ -1,13 +1,13 @@
 {
-  smoke = import ./smoke/test.nix;
-  pseudofiles = import ./pseudofiles/test.nix;
-  wlan = import ./wlan/test.nix;
-  pppoe = import ./pppoe/test.nix;
-  jffs2 = import ./jffs2/test.nix;
   ext4 = import ./ext4/test.nix;
-  min-copy-closure = import ./min-copy-closure/test.nix;
   fennel = import ./fennel/test.nix;
+  inout = import ./inout/test.nix;
+  jffs2 = import ./jffs2/test.nix;
+  min-copy-closure = import ./min-copy-closure/test.nix;
+  pppoe = import ./pppoe/test.nix;
+  pseudofiles = import ./pseudofiles/test.nix;
+  smoke = import ./smoke/test.nix;
   tftpboot = import ./tftpboot/test.nix;
   updown = import ./updown/test.nix;
-  inout = import ./inout/test.nix;
+  wlan = import ./wlan/test.nix;
 }
