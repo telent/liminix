5572c0ecb0b2d404053aa069de0f1c5217793a61
Daniel Barlow <dan@telent.net>
2024-12-09 23:36:22 +0000

    rewrite parts of inout test to be differently wrong

--- tests/inout/test.nix
+++ tests/inout/test.nix
@@ -18,13 +18,17 @@ mkfs.ext2 -L backup-disk ./vm/stick.e2fs
 dd if=/dev/zero of=./vm/stick.img bs=1M count=38
 dd if=./vm/stick.e2fs of=./vm/stick.img bs=512 seek=34 conv=notrunc
 parted -s ./vm/stick.img -- mklabel gpt mkpart backup-disk ext2 34s -0M
+sync
+cp  ./vm/stick.img  ./vm/stick.img.orig
 
 {
+
 
 ${img}/run.sh  --background ./vm --flag -device --flag usb-ehci,id=xhci --flag -drive  --flag if=none,id=usbstick,format=raw,file=$(pwd)/vm/stick.img
 expect ${./script.expect} late
 kill $(cat ./vm/pid)
 
+cp  ./vm/stick.img.orig ./vm/stick.img
 ${img}/run.sh  --background ./vm --flag -device --flag usb-ehci,id=xhci --flag -drive  --flag if=none,id=usbstick,format=raw,file=$(pwd)/vm/stick.img
 expect ${./script.expect} early
 
