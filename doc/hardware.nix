eb16a35ff3a28cbfb19bbd4df0f2a2fa43defd00
Daniel Barlow <dan@telent.net>
2025-04-08 21:24:31 +0100

    convert "supported hardware" to an appendix

--- doc/hardware.nix
+++ doc/hardware.nix
@@ -11,7 +11,7 @@ let
       d' = {
         description = ''
           ${n}
-          ${substring 0 (stringLength n) "********************************"}
+          ${substring 0 (stringLength n) "============================"}
         '';
       } // d;
     in
@@ -19,18 +19,6 @@ let
   ) devices;
 in
 writeText "hwdoc" ''
-  Supported hardware
-  ##################
-
-  For development, the `GL.iNet GL-MT300A <https://www.gl-inet.com/products/gl-mt300a/>`_
-  is an attractive choice as it has a builtin "debrick" procedure in the
-  boot monitor and is also comparatively simple to
-  attach serial cables to (soldering not required), so it
-  is lower-risk than some devices.
-
-  For a more powerful device, something with an ath10k would be the safe bet,
-  or the Linksys E8450 which seems popular in the openwrt community.
-
   ${lib.concatStringsSep "\n\n" texts}
 
 ''
