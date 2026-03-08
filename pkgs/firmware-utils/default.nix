dd3629ae8ed63cabaa2c7f79386c3cb6909b828f
Daniel Barlow <dan@telent.net>
2026-03-08 01:08:50 +0000

    update firmware-utils for compatibility with newer cmake

--- pkgs/firmware-utils/default.nix
+++ pkgs/firmware-utils/default.nix
@@ -13,8 +13,8 @@ stdenv.mkDerivation {
   src = fetchFromGitHub {
     owner = "openwrt";
     repo = "firmware-utils";
-    rev = "e87f23849790a7c77b4cd0e8ef0384da188174e5";
-    hash = "sha256-285Isf9sRuUt5S56SozgqpnS0+LOfnvpxpnWLwuWYUk=";
+    rev = "6a87eaf434cb89d4eba0b811a4b5d158fd9c519f";
+    hash = "";
   };
 
   nativeBuildInputs = [
